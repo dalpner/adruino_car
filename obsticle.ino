b7eb52e2cca566a682d28ed8a02a5086d482f45e
Dominic Leipner Alvarez <74786569+dalpner@users.noreply.github.com>
2025-04-25 13:23:07 +0200

    Add files via upload

---
+++ obsticle.ino
@@ -0,0 +1,60 @@
+const int A_1B = 5;
+const int A_1A = 6;
+const int B_1B = 9;
+const int B_1A = 10;
+
+const int rightIR = 7;
+
+void setup() {
+  Serial.begin(9600);
+
+  //motor
+  pinMode(A_1B, OUTPUT);
+  pinMode(A_1A, OUTPUT);
+  pinMode(B_1B, OUTPUT);
+  pinMode(B_1A, OUTPUT);
+
+  //IR obstacle
+  pinMode(rightIR, INPUT);
+
+}
+
+void loop() {
+
+  int right = digitalRead(rightIR); // 0: Obstructed   1: Empty
+  int speed = 150;
+
+  if (!right) {
+    backRight(speed);
+  } else {
+    moveForward(speed);
+  }
+}
+
+void moveForward(int speed) {
+  analogWrite(A_1B, 0);
+  analogWrite(A_1A, speed);
+  analogWrite(B_1B, speed);
+  analogWrite(B_1A, 0);
+}
+
+void moveBackward(int speed) {
+  analogWrite(A_1B, speed);
+  analogWrite(A_1A, 0);
+  analogWrite(B_1B, 0);
+  analogWrite(B_1A, speed);
+}
+
+void backLeft(int speed) {
+  analogWrite(A_1B, speed);
+  analogWrite(A_1A, 0);
+  analogWrite(B_1B, 0);
+  analogWrite(B_1A, 0);
+}
+
+void backRight(int speed) {
+  analogWrite(A_1B, 0);
+  analogWrite(A_1A, 0);
+  analogWrite(B_1B, 0);
+  analogWrite(B_1A, speed);
+}
